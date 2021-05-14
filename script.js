910bcbdef9fa89ff6799f194e400c09c7b12bd1e
unknown <sonofra98@gmail.com>
2021-05-14 12:19:41 +0300

    Changed index.html , style.css and script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+let color1 = document.getElementById("color1").value;
+let color2 = document.getElementById("color2").value;
+let container = document.getElementsByClassName("container").style.background;
+container.value =
