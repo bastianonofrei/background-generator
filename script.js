351a0ffbdef58a4a892c3fa365ac4004fb640de2
unknown <sonofra98@gmail.com>
2021-05-14 13:19:41 +0300

    added button. created changeColor() , created header , created randomization for font family and font sizes for the paragraph

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-let color1 = document.getElementById("color1").value;
-let color2 = document.getElementById("color2").value;
-let container = document.getElementsByClassName("container").style.background;
-container.value =
+let paragraph = document.getElementById("colorValue");
+let fontSize = 0;
+let fontfamilies = ['Arial', 'monospace', 'Helvetica', 'Impact'];
+let randomFont = 0;
+let btn = document.getElementById("btn");
+function changeColor() {
+    while (fontSize < 10) {
+        fontSize = Math.random() * 40;
+    }
+    randomFont = Math.random() * 4;
+    randomFont = randomFont.toFixed(0);
+    let num1 = Math.random() * 255;
+    num1 = num1.toFixed(0);
+    let num2 = Math.random() * 255;
+    num2 = num2.toFixed(0);
+    let num3 = Math.random() * 255;
+    num3 = num3.toFixed(0);
+    console.log("button clicked");
+    paragraph.style.fontSize = `${fontSize}px`;
+    paragraph.style.fontFamily = `${fontfamilies[randomFont]}`;
+    paragraph.textContent = `RGB(${num1},${num2},${num3})`;
+    document.body.style.backgroundColor = `rgb(${num1},${num2},${num3})`;
+    btn.style.fontFamily = `${fontfamilies[randomFont]}`;
+    fontSize = Math.random() * 40;
+}
+
